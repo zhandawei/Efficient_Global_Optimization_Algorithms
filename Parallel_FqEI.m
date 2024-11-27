5dc55d24f6e4e6602bf93d9363545a2fc44d9696
Dawei Zhan <zhandawei@users.noreply.github.com>
2024-11-27 12:33:02 +0800

    Update Parallel_FqEI.m

--- Parallel_FqEI.m
+++ Parallel_FqEI.m
@@ -1,43 +1,44 @@
+%-------------------------------------------------------------------------
+
 clearvars; close all;
 % setting of the problem
 fun_name = 'Fun_Rosenbrock';
 num_vari = 10;
 lower_bound = -2.048*ones(1,num_vari);
 upper_bound = 2.048*ones(1,num_vari);
-% the number of initial design points
+% number of initial design points
 num_initial = 20;
 % maximum number of evaluations
 max_evaluation = 100;
-% the number of points selected in each iteration
-num_q = 2;
+% batch size
+num_q = 4;
 % initial design points using Latin hypercube sampling method
 sample_x = lower_bound + (upper_bound-lower_bound).*lhsdesign(num_initial,num_vari,'criterion','maximin','iterations',1000);
 sample_y = feval(fun_name,sample_x);
-% the current iteration and evaluation
 evaluation = size(sample_x,1);
 iteration = 0;
-% the current best solution
+% current best solution
 fmin = min(sample_y);
 % print the current information to the screen
-fprintf('EGO-FqEI on %d-D %s function, iteration: %d, evaluation: %d, current best solution: %f\n',num_vari,fun_name,iteration,evaluation,fmin);
+fprintf('Fast Multi-Point Expected Improvement on %d-D %s function, iteration: %d, evaluation: %d, current best solution: %f\n',num_vari,fun_name,iteration,evaluation,fmin);
 % the iteration
 while evaluation < max_evaluation
-    % build (or rebuild) the initial Kriging model
+    % build the Kriging model
     kriging_model = Kriging_Train(sample_x,sample_y,lower_bound,upper_bound,1*ones(1,num_vari),0.001*ones(1,num_vari),1000*ones(1,num_vari));
-    % maximize the qEI function
+    % maximize the FqEI function using GA
     [best_x,max_EI]= Optimizer_GA(@(x)-Infill_FqEI(x,kriging_model,fmin),num_vari*num_q,repmat(lower_bound,1,num_q),repmat(upper_bound,1,num_q),50,100);
     infill_x = reshape(best_x,num_vari,[])';
-    % evaluating the candidate with the real function
+    % evaluate the query points with the real function
     infill_y = feval(fun_name,infill_x);
-    % add the new point to design set
+    % add the new points to design set
     sample_x = [sample_x;infill_x];
     sample_y = [sample_y;infill_y];
-    % updating some parameters
+    % update some parameters
     evaluation = size(sample_x,1);
     iteration = iteration + 1;
     fmin = min(sample_y);
     % print the current information to the screen
-    fprintf('EGO-FqEI on %d-D %s function, iteration: %d, evaluation: %d, current best solution: %f\n',num_vari,fun_name,iteration,evaluation,fmin);
+    fprintf('Fast Multi-Point Expected Improvement on %d-D %s function, iteration: %d, evaluation: %d, current best solution: %f\n',num_vari,fun_name,iteration,evaluation,fmin);
 end
 
 
